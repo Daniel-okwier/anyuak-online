0e9154877dad90f72734722eeef082123ae711e6
Daniel-okwier <coderdani229@gmail.com>
2025-04-20 02:59:45 +0300

    Update middleware

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,17 +1,19 @@
 const multer = require('multer');
 const path = require('path');
 
+// Define storage for uploaded files
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const fileExtension = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
-  },
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../uploads/')); 
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    },
 });
 
-const upload = multer({ storage: storage });
-
-module.exports = upload;
+// Filter for only images and videos
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
+        cb(null, true);
+    }};
